fd1f882636d7814c27315fd438750139a704c0a4
Amit Upreti <a.u.aua937@gmail.com>
2022-10-16 13:45:13 -0700

    implemented class based add to cart instead of function based

--- carts/urls.py
+++ carts/urls.py
@@ -4,6 +4,6 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('', views.CartDetailView.as_view(), name='cart_detail'),
-    path('add/<int:product_id>/', views.add_to_cart, name='add_cart'),
+    path('', views.CartView.as_view(), name='cart_detail'),
+    path('add/<int:product_id>/', views.AddToCartView.as_view(), name='add_cart'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,6 @@
+import decimal
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, TemplateView, CreateView
 from django.views import View
@@ -6,32 +9,29 @@ from carts.models import Cart, CartItem
 from store.models import Product
 from category.models import Category
 
+from eStore.utils import _get_cart_id
 
-class CartDetailView(TemplateView):
+
+class CartView(TemplateView):
     model = Cart
     template_name = 'store/cart.html'
-    # products = Cart.objects.all().filter(is_available=True)
-
-    # all_categories = Category.objects.all()
-
-    # def get(self, request, *args, **kwargs):
-    #     category_slug = self.kwargs.get('slug')
-    #     if category_slug:
-    #         categories = get_object_or_404(Category, slug=category_slug)
-    #         self.products = self.products.filter(category=categories)
-    #     return super().get(request, *args, **kwargs)
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['products'] = self.products
-    #     # context['categories'] = self.all_categories
-    #     return context
-
-
-class CartView(View):
-    model = Cart
-    # template_name = 'store/cart.html'
     fields = ['quantity']
+    context_object_name = 'cart'
+    tax = decimal.Decimal(0.02)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            cart = Cart.objects.get(cart_id=_get_cart_id(self.request))
+            all_items = CartItem.objects.filter(is_active=True, cart=cart)
+            context['cart_items'] = all_items
+            context['total_price_before_tax'] = round(sum(item.sub_total() for item in all_items), 2)
+            context['total_tax'] = round(sum(item.sub_total() * self.tax for item in all_items), 2)
+            context['total_price_after_tax'] = context['total_price_before_tax'] + context['total_tax']
+            context['cart_size'] = all_items.count()
+        except ObjectDoesNotExist:
+            pass
+        return context
 
 
 # class AddToCartView(TemplateView):
@@ -80,35 +80,32 @@ class CartView(View):
 #         return cart
 #
 
-def add_to_cart(request, *args, **kwargs):
-    product_id = kwargs.get('product_id')
-    product = Product.objects.get(id=product_id)
-    try:
-        cart = Cart.objects.get(cart_id=_get_cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id=_get_cart_id(request)
-        )
-        cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        if cart_item.quantity < cart_item.product.stock:
-            cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            product=product,
-            quantity=1,
-            cart=cart
-        )
-        cart_item.save()
+class AddToCartView(TemplateView):
+    # model = Cart
 
-    return redirect('carts:cart_detail')
+    def get(self, request, *args, **kwargs):
+        product_id = kwargs.get('product_id')
+        product = Product.objects.get(id=product_id)
+        try:
+            cart = Cart.objects.get(cart_id=_get_cart_id(request))
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create(
+                cart_id=_get_cart_id(request)
+            )
+            cart.save()
 
+        try:
+            cart_item = CartItem.objects.get(product=product, cart=cart)
+            if cart_item.quantity < cart_item.product.stock:
+                cart_item.quantity += 1
+            cart_item.save()
+        except CartItem.DoesNotExist:
+            cart_item = CartItem.objects.create(
+                product=product,
+                quantity=1,
+                cart=cart
+            )
+            cart_item.save()
 
-def _get_cart_id(request):
-    cart = request.session.session_key
-    if not cart:
-        cart = request.session.create()
-    return cart
+        return redirect('carts:cart_detail')
