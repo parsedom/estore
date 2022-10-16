25068c6336bcb2b8d2c74b35d6d69d6b4a208183
Amit Upreti <a.u.aua937@gmail.com>
2022-10-15 20:46:41 -0700

    working on store

--- eStore/views.py
+++ eStore/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from store.models import Product
+
+
+class HomeView(ListView):
+    model = Product
+    template_name = 'home.html'
+    products = Product.objects.all().filter(is_available=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = self.products
+        return context
 
 
 def home(request):
